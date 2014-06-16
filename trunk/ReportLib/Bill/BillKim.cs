e44de9cfa51b3159cfee8dde7f2b2e0b25edc40f
datuanitc@gmail.com <datuanitc@gmail.com@f52bd7ea-2980-a848-c339-e30612b6e088>
2014-06-16 14:44:13 +0000

    Created by TuanDa

---
+++ trunk/ReportLib/Bill/BillKim.cs
@@ -0,0 +1,27 @@
+namespace ReportLib.Bill
+{
+    using System;
+    using System.ComponentModel;
+    using System.Drawing;
+    using System.Windows.Forms;
+    using Telerik.Reporting;
+    using Telerik.Reporting.Drawing;
+
+    /// <summary>
+    /// Summary description for Bill.
+    /// </summary>
+    public partial class BillKim : Telerik.Reporting.Report
+    {
+        public BillKim()
+        {
+            //
+            // Required for telerik Reporting designer support
+            //
+            InitializeComponent();
+
+            //
+            // TODO: Add any constructor code after InitializeComponent call
+            //
+        }
+    }
+}
